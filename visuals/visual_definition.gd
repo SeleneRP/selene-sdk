b9da388d09b349ab5b260f04a65e9bb07b4a496c
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2024-07-08 07:42:05 +0200

    refactor: Require a scene in visual definitions for more stable and resilient API

--- visuals/visual_definition.gd
+++ visuals/visual_definition.gd
@@ -2,12 +2,4 @@
 extends Resource
 class_name VisualDefinition
 
-@export_group("Scene")
 @export var scene: PackedScene
-
-@export_group("Sprite2D")
-@export var texture: Texture2D
-
-@export_group("AnimatedSprite2D")
-@export var sprite_frames: SpriteFrames
-@export var animation: String
