99d3ffbe0236be60d67e274a6aa4e091b28ac0a5
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2024-11-09 22:42:02 +0100

    Readd get_default_lua_vm

--- sdk_bindings.gd
+++ sdk_bindings.gd
@@ -5,3 +5,6 @@ static var bindings_impl = null
 
 static func get_impl_script(p_name: String):
 	return bindings_impl.get_impl_script(p_name)
+
+static func get_default_lua_vm():
+	return bindings_impl.get_default_lua_vm()
