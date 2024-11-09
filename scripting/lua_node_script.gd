a2e107b386466789035ac3ccebdc01b658568277
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2024-11-09 09:11:50 +0100

    Split API from implementation for LuaNodeScript

--- scripting/lua_node_script.gd
+++ scripting/lua_node_script.gd
@@ -4,34 +4,13 @@ extends Node2D
 @export var lua_module_path: String
 @export var bindings: Dictionary = {}
 
-var lua_module: Variant
-var state: Variant
-var lua_bindings = {}
-
 func _ready():
-	lua_module = SdkBindings.require_lua(lua_module_path)
-	if not lua_module:
-		return
-
-	for key in bindings:
-		var value = bindings[key]
-		if value.begins_with("res://"):
-			value = load(value)
-		lua_bindings[key] = value
-
-	if "initial_state" in lua_module:
-		state = lua_module["initial_state"].call()
-
-	if "ready" in lua_module:
-		SdkBindings.handle_lua_error(lua_module["ready"].call(self, state, lua_bindings))
+	var _lua_module_path = lua_module_path
+	var _bindings = bindings.duplicate(true)
+	self.script = SdkBindings.get_lua_node_script()
+	self.lua_module_path = lua_module_path
+	self.bindings = _bindings
+	self._ready()
 
 func _process(delta: float):
-	# TODO this is just for testing:
-	var mouse_pos = get_global_mouse_position()
-	var tiled_mouse_pos = Vector2(floor(mouse_pos.x / 76) * 76, floor(mouse_pos.y / 37) * 37)
-	global_position = tiled_mouse_pos + Vector2(37, 18.5)
-
-	if lua_module and "process" in lua_module:
-		var new_state = SdkBindings.handle_lua_error(lua_module["process"].call(null, state, lua_bindings, delta))
-		if new_state is not LuaError:
-			state = new_state
+	pass
--- sdk_bindings.gd
+++ sdk_bindings.gd
@@ -3,8 +3,14 @@ class_name SdkBindings
 	
 static var bindings_impl = null
 
+static func get_default_lua_vm():
+	return bindings_impl.get_default_lua_vm()
+
 static func require_lua(module_path: String):
 	return bindings_impl.require_lua(module_path)
 
 static func handle_lua_error(error: Variant):
 	return bindings_impl.handle_lua_error(error)
+
+static func get_lua_node_script():
+	return bindings_impl.get_lua_node_script()
